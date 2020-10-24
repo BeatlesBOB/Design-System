b7f2eebd67ece414b35ae3a9d2cebbdcfe510975
BeatlesBOB <38206766+BeatlesBOB@users.noreply.github.com>
2020-10-24 18:32:38 +0200

    Signin animation

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,35 +1,43 @@
 $(document).ready(function() {
 
-    $("#formStep").validate({
-        rules: {
-           "montextarea":{
-              "required": true,
-              "minlength": 2,
-              "maxlength": 60000
-           },
-           "monemail": {
-              "email": true,
-              "maxlength": 255
-           },
-           "montelephone": {
-              "required": true
-           }
-           
-        },
+    $("#signinForm").validate({
+      errorPlacement: function($error, $element) {
+         $error.appendTo(".errorMessage");
+         $element.addClass("his-invalidated")
+       },
+      rules: {
+         email: {
+           required: true,
+           email: true
+         },
+         password: {
+           required: true,
+           minlength: 5
+         }
+       },
         messages: {
-            firstname: "Please enter your firstname",
-            lastname: "Please enter your lastname",
             password: {
-              required: "Please provide a password",
-              minlength: "Your password must be at least 5 characters long"
+              required: "<p class='text-error text--light--italic text-0p'>Please provide a password <p>",
+              minlength: "<p class='text-error text--light--italic text-0p'>Your password must be at least 5 characters long<p>"
             },
-            email: "Please enter a valid email address"
+            email: "<p class='text-error text--light--italic text-0p'>Please enter a valid email address</p>"
         },
-        invalidHandler: function(form){  
-            console.log("AAAA")
+        invalidHandler: function(form){ 
+         animateCSS('.a-input', 'shakeX');
+
         }
     })
     
  });
 
 
+
+ function animateCSS(element,animation){
+   var prefix = 'animate__';
+   document.querySelectorAll(element).forEach(function(el){
+      el.classList.add('animate__animated',prefix+animation );
+      el.addEventListener('animationend', () => {
+         el.classList.remove('animate__animated', prefix+animation);
+       });   
+    });
+ }
